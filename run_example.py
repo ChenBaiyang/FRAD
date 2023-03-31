38de46236a03b651d608ebac3bbfd329a65163fd
FRAD <farstars@qq.com>
2023-03-31 09:45:59 +0800

    Revert "Revert "Init""
    This reverts commit e38dad69ad0c0262d615cdaee2bedd68657de690.

---
+++ run_example.py
@@ -0,0 +1,22 @@
+import numpy as np
+from sklearn.preprocessing import minmax_scale
+import torch
+torch.set_default_dtype(torch.float32)
+from models import FRAD_example
+
+if __name__ == "__main__":
+    data = np.array(
+           [[2, 0.2, 1],
+            [1, 0.1, 2],
+            [9, 0.5, 2],
+            [5, 0.8, 1],
+            [7, 0.2, 2],
+            [3, 0.7, 1]])
+
+    # column one is a nominal attribute, which doesnot apply to min-max.
+    data[:, :-1] = minmax_scale(data[:, :-1])
+    print(data)
+
+    out_factor = FRAD_example(data, dim=3, gamma=0.4)
+    print(out_factor)
+
